dbb24d017ed5af4d08584ec36b5465877f70b76b
JAICHANGPARK <aristojeff@gmail.com>
2025-10-18 09:58:56 +0900

    Update real_esate_main_screen.dart

--- lib/ep2401_real_estate_app/ui/real_esate_main_screen.dart
+++ lib/ep2401_real_estate_app/ui/real_esate_main_screen.dart
@@ -1,2 +1,22 @@
 import 'package:flutter/material.dart';
 
+class RealEsateMainScreen extends StatefulWidget {
+  const RealEsateMainScreen({super.key});
+
+  @override
+  State<RealEsateMainScreen> createState() => _RealEsateMainScreenState();
+}
+
+class _RealEsateMainScreenState extends State<RealEsateMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
