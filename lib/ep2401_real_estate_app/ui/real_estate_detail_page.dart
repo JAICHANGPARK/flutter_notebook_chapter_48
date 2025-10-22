e54e8efdc4c35653d86a515d47adaceede6ed50f
JAICHANGPARK <aristojeff@gmail.com>
2025-10-22 18:50:06 +0900

    Update real_estate_detail_page.dart

--- lib/ep2401_real_estate_app/ui/real_estate_detail_page.dart
+++ lib/ep2401_real_estate_app/ui/real_estate_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class RealEstateDetailPage extends StatefulWidget {
   const RealEstateDetailPage({super.key});
 
@@ -12,20 +11,24 @@ class _RealEstateDetailPageState extends State<RealEstateDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
-          ],
+          Container(height: 320),
+          Expanded(
+            child: SingleChildScrollView(child: Column(children: [])),
+          ),
+        ],
       ),
-      bottomNavigationBar: Container(
-        height: 100,
-        child: Row(
-          children: [
-
-          ],
-        ),
-      ),
+      bottomNavigationBar: Container(height: 100, child: Row(children: [
+        Expanded(child: Column(children: [
+          
+        ],)),
+        Container(
+          decoration: ShapeDecoration(shape: StadiumBorder(),),
+        )
+      ])),
     );
   }
 }
