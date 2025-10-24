db4111f0e0c6a950e37b87941d9305dfccc394b6
JAICHANGPARK <aristojeff@gmail.com>
2025-10-24 17:56:08 +0900

    Update food_recipe_main_menu_page.dart

--- lib/ep2407_food_recipe_app/ui/food_recipe_main_menu_page.dart
+++ lib/ep2407_food_recipe_app/ui/food_recipe_main_menu_page.dart
@@ -11,7 +11,19 @@ class _FoodRecipeMainMenuPageState extends State<FoodRecipeMainMenuPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [Positioned(child: Container())]),
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 16,
+            child: Container(
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Color.fromRGBO(27, 27, 27, 1),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
