253b74a47551c411bbc873663a4f657f7d81f446
JAICHANGPARK <aristojeff@gmail.com>
2025-10-25 09:16:53 +0900

    Update food_recipe_main_menu_page.dart

--- lib/ep2407_food_recipe_app/ui/food_recipe_main_menu_page.dart
+++ lib/ep2407_food_recipe_app/ui/food_recipe_main_menu_page.dart
@@ -74,22 +74,25 @@ class _FoodRecipeMainMenuPageState extends State<FoodRecipeMainMenuPage> {
                       color: Colors.white.withValues(alpha: .1),
                     ),
                   ),
-                  SizedBox(
-                    height: 42,
-                  ),
+                  SizedBox(height: 42),
                   Column(
                     spacing: 12,
                     children: [
                       Text(
                         "You haven't created a shopping list yet",
-                        style: TextStyle(color: Colors.white),
+                        style: TextStyle(color: Colors.white, fontSize: 16),
                       ),
                       Container(
                         decoration: ShapeDecoration(
                           shape: StadiumBorder(),
                           color: Color.fromRGBO(242, 78, 4, 1),
                         ),
-                        child: Center(child: Text("Create a shoppping list")),
+                        child: Center(
+                          child: Text(
+                            "Create a shoppping list",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ),
                       ),
                     ],
                   ),
