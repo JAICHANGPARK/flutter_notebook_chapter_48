d63c88926aa754989edcd46512159ff7f511b146
JAICHANGPARK <aristojeff@gmail.com>
2025-10-27 17:18:15 +0900

    Create food_recipe_my_menus_page.dart

---
+++ lib/ep2407_food_recipe_app/ui/food_recipe_my_menus_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FoodRecipeMyMenusPage extends StatefulWidget {
+  const FoodRecipeMyMenusPage({super.key});
+
+  @override
+  State<FoodRecipeMyMenusPage> createState() => _FoodRecipeMyMenusPageState();
+}
+
+class _FoodRecipeMyMenusPageState extends State<FoodRecipeMyMenusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
