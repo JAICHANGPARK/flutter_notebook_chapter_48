35bc65441c2de094983ae6233f7edc0c3d81cbf7
JAICHANGPARK <aristojeff@gmail.com>
2025-10-27 17:24:56 +0900

    Update food_recipe_my_menus_page.dart

--- lib/ep2407_food_recipe_app/ui/food_recipe_my_menus_page.dart
+++ lib/ep2407_food_recipe_app/ui/food_recipe_my_menus_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FoodRecipeMyMenusPage extends StatefulWidget {
   const FoodRecipeMyMenusPage({super.key});
 
@@ -12,13 +11,22 @@ class _FoodRecipeMyMenusPageState extends State<FoodRecipeMyMenusPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      spacing: 24,
       children: [
         Container(
           height: 58,
-          decoration: ShapeDecoration(shape: StadiumBorder(),
-          color: Colors.white12,
+          decoration: ShapeDecoration(
+            shape: StadiumBorder(),
+            color: Colors.white12,
           ),
-        )
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "Your menu is empty. Add the recipes you want to plan",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
       ],
     );
   }
