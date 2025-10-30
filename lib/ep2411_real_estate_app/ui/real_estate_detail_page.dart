c5d122f7af4db654d9bdf7db68617d9f8d3186b7
JAICHANGPARK <aristojeff@gmail.com>
2025-10-30 18:47:36 +0900

    Update real_estate_detail_page.dart

--- lib/ep2411_real_estate_app/ui/real_estate_detail_page.dart
+++ lib/ep2411_real_estate_app/ui/real_estate_detail_page.dart
@@ -10,6 +10,12 @@ class RealEstateDetailPage extends StatefulWidget {
 class _RealEstateDetailPageState extends State<RealEstateDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
