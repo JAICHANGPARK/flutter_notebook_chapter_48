cf7a4f987241ac1973b5b7537f58b9d753d3ac82
JAICHANGPARK <aristojeff@gmail.com>
2025-10-30 18:53:54 +0900

    Update real_estate_detail_page.dart

--- lib/ep2411_real_estate_app/ui/real_estate_detail_page.dart
+++ lib/ep2411_real_estate_app/ui/real_estate_detail_page.dart
@@ -15,6 +15,7 @@ class _RealEstateDetailPageState extends State<RealEstateDetailPage> {
         children: [
           Positioned.fill(
             child: ListView.separated(
+              padding: EdgeInsets.zero,
               itemCount: 10,
               itemBuilder: (context, index) {
                 return Container(
