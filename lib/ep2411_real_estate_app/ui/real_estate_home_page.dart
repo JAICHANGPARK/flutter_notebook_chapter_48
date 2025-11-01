d1e1051327520f3e44d76b26df5564bbcad254e8
JAICHANGPARK <aristojeff@gmail.com>
2025-11-01 09:47:42 +0900

    Update real_estate_home_page.dart

--- lib/ep2411_real_estate_app/ui/real_estate_home_page.dart
+++ lib/ep2411_real_estate_app/ui/real_estate_home_page.dart
@@ -197,22 +197,25 @@ class _RealEstateHomePageState extends State<RealEstateHomePage> {
               ),
             ),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              decoration: ShapeDecoration(
-                shape: StadiumBorder(),
-                color: Colors.white,
-              ),
-              padding: EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white,
-                    child: Icon(Icons.home_filled),
-                  ),
-                ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 32),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: ShapeDecoration(
+                  shape: StadiumBorder(),
+                  color: Colors.white,
+                ),
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                      child: Icon(Icons.home_filled),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
