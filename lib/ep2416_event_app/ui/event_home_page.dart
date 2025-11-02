acfd44089d3d090a5ed72a1875df07240f19d4e3
JAICHANGPARK <aristojeff@gmail.com>
2025-11-02 11:02:45 +0900

    Update event_home_page.dart

---
+++ lib/ep2416_event_app/ui/event_home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class EventHomePage extends StatefulWidget {
+  const EventHomePage({super.key});
+
+  @override
+  State<EventHomePage> createState() => _EventHomePageState();
+}
+
+class _EventHomePageState extends State<EventHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
