993899c9cec65d4ae02336e995c711437661d83b
JAICHANGPARK <aristojeff@gmail.com>
2025-11-02 11:05:13 +0900

    Update event_app.dart

--- lib/ep2416_event_app/event_app.dart
+++ lib/ep2416_event_app/event_app.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_48/ep2416_event_app/ui/event_home_page.dart';
 
 class EventApp extends StatelessWidget {
   const EventApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-    );
+    return MaterialApp(home: EventHomePage());
   }
 }
