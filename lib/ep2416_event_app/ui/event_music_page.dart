e1d75a11c9447d497213b986cb1d864d7b7bfe02
JAICHANGPARK <aristojeff@gmail.com>
2025-11-04 18:21:38 +0900

    Create event_music_page.dart

---
+++ lib/ep2416_event_app/ui/event_music_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EventMusicPage extends StatefulWidget {
+  const EventMusicPage({super.key});
+
+  @override
+  State<EventMusicPage> createState() => _EventMusicPageState();
+}
+
+class _EventMusicPageState extends State<EventMusicPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
