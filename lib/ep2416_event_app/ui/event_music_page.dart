155a794bed4926503b0acaa28d45c275643d725b
JAICHANGPARK <aristojeff@gmail.com>
2025-11-04 18:21:57 +0900

    Update event_music_page.dart

--- lib/ep2416_event_app/ui/event_music_page.dart
+++ lib/ep2416_event_app/ui/event_music_page.dart
@@ -11,6 +11,6 @@ class EventMusicPage extends StatefulWidget {
 class _EventMusicPageState extends State<EventMusicPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(padding: EdgeInsets.symmetric(horizontal: 16),);
   }
 }
