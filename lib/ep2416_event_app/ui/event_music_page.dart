9abaa2665c509ba29320e3cd644ab81f65471487
JAICHANGPARK <aristojeff@gmail.com>
2025-11-04 18:24:31 +0900

    Update event_music_page.dart

--- lib/ep2416_event_app/ui/event_music_page.dart
+++ lib/ep2416_event_app/ui/event_music_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class EventMusicPage extends StatefulWidget {
   const EventMusicPage({super.key});
 
@@ -11,11 +10,14 @@ class EventMusicPage extends StatefulWidget {
 class _EventMusicPageState extends State<EventMusicPage> {
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.symmetric(horizontal: 16),
-    child: Column(
-      children: [
-
-      ],
-    ),);
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          Container(height: 100, child: Placeholder()),
+          Expanded(child: ListView.builder(itemBuilder: (context, index) {})),
+        ],
+      ),
+    );
   }
 }
