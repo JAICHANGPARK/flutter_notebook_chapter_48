7fba384e1d08542ff9b3b43b98a8f4524cc03f76
JAICHANGPARK <aristojeff@gmail.com>
2025-11-05 19:00:12 +0900

    Update promotion_app.dart

--- lib/ep2419_promotion_app/promotion_app.dart
+++ lib/ep2419_promotion_app/promotion_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_48/ep2419_promotion_app/ui/promotion_page.dart';
 
 class PromotionApp extends StatelessWidget {
   const PromotionApp({super.key});
@@ -6,7 +7,7 @@ class PromotionApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: PromotionPage(),
     );
   }
 }
