d759dffa377ab2ab8eb030a577ffae1d543e1f70
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 11:42:29 +0900

    Update book_app.dart

--- lib/ep2426_book_app/book_app.dart
+++ lib/ep2426_book_app/book_app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_48/ep2426_book_app/ui/book_start_page.dart';
 
 class BookApp extends StatelessWidget {
   const BookApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: BookStartPage(),
+    );
   }
 }
