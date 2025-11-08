675933b9e3bc746933749111176a4ffef921751b
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 11:51:01 +0900

    Update book_start_page.dart

--- lib/ep2426_book_app/ui/book_start_page.dart
+++ lib/ep2426_book_app/ui/book_start_page.dart
@@ -11,63 +11,68 @@ class _BookStartPageState extends State<BookStartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          spacing: 16,
-          children: [
-            SizedBox(height: 16),
-            Container(
-              height: 220,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
+      body: Stack(
+        children: [
+          Positioned(child: Container(decoration: BoxDecoration(),),),
+          SafeArea(
+            child: Column(
+              spacing: 16,
+              children: [
+                SizedBox(height: 16),
+                Container(
+                  height: 220,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
 
-                itemBuilder: (context, index) => Container(
-                  width: 160,
-                  margin: EdgeInsets.only(right: 16),
-                  child: Placeholder(),
+                    itemBuilder: (context, index) => Container(
+                      width: 160,
+                      margin: EdgeInsets.only(right: 16),
+                      child: Placeholder(),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Container(
-              height: 220,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
+                Container(
+                  height: 220,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
 
-                itemBuilder: (context, index) => Container(
-                  width: 160,
-                  margin: EdgeInsets.only(right: 16),
-                  child: Placeholder(),
+                    itemBuilder: (context, index) => Container(
+                      width: 160,
+                      margin: EdgeInsets.only(right: 16),
+                      child: Placeholder(),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-           Spacer(),
-            Text("Learn more in less time", style: TextStyle(fontSize: 28)),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Text(
-                "Enjoy quick insights, simple takeaways, and smarter reading made easy.",
-                style: TextStyle(fontSize: 16,),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16,),
-              padding: EdgeInsets.symmetric(vertical: 16),
-              decoration: BoxDecoration(color: Colors.black),
-              child: Center(
-                child: Text(
-                  "Get Started",
-                  style: TextStyle(color: Colors.white,
-                  fontSize: 16),
+               Spacer(),
+                Text("Learn more in less time", style: TextStyle(fontSize: 28)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Text(
+                    "Enjoy quick insights, simple takeaways, and smarter reading made easy.",
+                    style: TextStyle(fontSize: 16,),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 16,),
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  decoration: BoxDecoration(color: Colors.black),
+                  child: Center(
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(color: Colors.white,
+                      fontSize: 16),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("I already have an account"),
+                ),
+              ],
             ),
-            TextButton(
-              onPressed: () {},
-              child: Text("I already have an account"),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
