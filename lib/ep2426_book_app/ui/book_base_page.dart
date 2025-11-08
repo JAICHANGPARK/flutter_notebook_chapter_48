947d7cf2a4da77c40151e7fee75899a7fac60e35
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 12:26:05 +0900

    Update book_base_page.dart

--- lib/ep2426_book_app/ui/book_base_page.dart
+++ lib/ep2426_book_app/ui/book_base_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class BookBasePage extends StatefulWidget {
-  const BookBasePage({super.key});
+  Widget child;
+  const BookBasePage({super.key ,this.child,});
 
   @override
   State<BookBasePage> createState() => _BookBasePageState();
@@ -10,6 +11,28 @@ class BookBasePage extends StatefulWidget {
 class _BookBasePageState extends State<BookBasePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+
+                  colors: [
+                    Color.fromRGBO(254, 247, 236, 1),
+                    Colors.white,
+                    Colors.white,
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
