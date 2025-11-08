2a354ceb9fb2ecde1156c624c8320fdc76db5a33
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 12:25:31 +0900

    Update book_base_page.dart

--- lib/ep2426_book_app/ui/book_base_page.dart
+++ lib/ep2426_book_app/ui/book_base_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class BookBasePage extends StatefulWidget {
+  const BookBasePage({super.key});
+
+  @override
+  State<BookBasePage> createState() => _BookBasePageState();
+}
+
+class _BookBasePageState extends State<BookBasePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
