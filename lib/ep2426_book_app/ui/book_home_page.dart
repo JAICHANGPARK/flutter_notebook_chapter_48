cf0b04c0756cbf60aaa22ae60d77bd99d1d231ba
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 11:53:38 +0900

    Update book_home_page.dart

--- lib/ep2426_book_app/ui/book_home_page.dart
+++ lib/ep2426_book_app/ui/book_home_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class BookHomePage extends StatefulWidget {
+  const BookHomePage({super.key});
+
+  @override
+  State<BookHomePage> createState() => _BookHomePageState();
+}
+
+class _BookHomePageState extends State<BookHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
