7a7224687103d8a759f36abb07a085e31d25839e
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 11:06:41 +0900

    Update promo_products_modal.dart

---
+++ lib/ep2419_promotion_app/ui/widget/promo_products_modal.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PromoProductsModal extends StatefulWidget {
+  const PromoProductsModal({super.key});
+
+  @override
+  State<PromoProductsModal> createState() => _PromoProductsModalState();
+}
+
+class _PromoProductsModalState extends State<PromoProductsModal> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
