6d05b04a1786f178645489c65b202c6757241af9
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 12:23:07 +0900

    Update book_home_page.dart

--- lib/ep2426_book_app/ui/book_home_page.dart
+++ lib/ep2426_book_app/ui/book_home_page.dart
@@ -11,6 +11,15 @@ class _BookHomePageState extends State<BookHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: CircleAvatar(),
+        title: Text("BookApp"),
+        actions: [
+          CircleAvatar(
+            child: Badge(child: Icon(Icons.notifications_active_outlined)),
+          ),
+        ],
+      ),
       body: SafeArea(child: Column(children: [])),
     );
   }
