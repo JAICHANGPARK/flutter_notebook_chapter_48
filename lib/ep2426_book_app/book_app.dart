35fdbf4271abf843dbb0a8705f401d503b6c1fc2
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 11:41:22 +0900

    Update book_app.dart

--- lib/ep2426_book_app/book_app.dart
+++ lib/ep2426_book_app/book_app.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
