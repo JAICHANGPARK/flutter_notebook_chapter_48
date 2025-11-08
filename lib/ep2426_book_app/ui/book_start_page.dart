c9c3e21e0466f69c2036d06fb2732fa70a8469fe
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 11:42:05 +0900

    Create book_start_page.dart

---
+++ lib/ep2426_book_app/ui/book_start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BookStartPage extends StatefulWidget {
+  const BookStartPage({super.key});
+
+  @override
+  State<BookStartPage> createState() => _BookStartPageState();
+}
+
+class _BookStartPageState extends State<BookStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
