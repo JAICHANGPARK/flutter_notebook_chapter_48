228536a7101ab9b7cd61a088ee35cd6dc66fd0a9
JAICHANGPARK <aristojeff@gmail.com>
2025-11-08 11:04:19 +0900

    Create promo_create_discount_modal_widget.dart

---
+++ lib/ep2419_promotion_app/ui/widget/promo_create_discount_modal_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PromoCreateDiscountModalWidget extends StatefulWidget {
+  const PromoCreateDiscountModalWidget({super.key});
+
+  @override
+  State<PromoCreateDiscountModalWidget> createState() => _PromoCreateDiscountModalWidgetState();
+}
+
+class _PromoCreateDiscountModalWidgetState extends State<PromoCreateDiscountModalWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
