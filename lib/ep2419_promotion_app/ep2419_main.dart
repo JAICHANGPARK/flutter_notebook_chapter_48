dad1014dd56a0276fb96a56016825f64b4d193ef
Dreamwalker <aristojeff@gmail.com>
2025-11-13 00:55:57 +0900

    Update ep2419_main.dart

--- lib/ep2419_promotion_app/ep2419_main.dart
+++ lib/ep2419_promotion_app/ep2419_main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_48/ep2419_promotion_app/promotion_app.dart';
 
-
 void main() {
- runApp(PromotionApp());
+  runApp(PromotionApp());
 }
