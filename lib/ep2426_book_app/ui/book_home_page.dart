94501ecbd7e7e7e29472dac1c6d96284679ee7b6
Dreamwalker <aristojeff@gmail.com>
2025-11-14 01:58:00 +0900

    Update book_home_page.dart

--- lib/ep2426_book_app/ui/book_home_page.dart
+++ lib/ep2426_book_app/ui/book_home_page.dart
@@ -16,7 +16,6 @@ class _BookHomePageState extends State<BookHomePage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         surfaceTintColor: Colors.transparent,
-        // leadingWidth: 42,
         actionsPadding: EdgeInsets.only(right: 16),
 
         leading: Padding(
