9b236c61606d730c784928a69a3007a5720250e5
JAICHANGPARK <aristojeff@gmail.com>
2025-11-17 17:09:00 +0900

    Update barber_main_page.dart

--- lib/ep2431_barber_booking_app/ui/barber_main_page.dart
+++ lib/ep2431_barber_booking_app/ui/barber_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class BarberMainPage extends StatefulWidget {
   const BarberMainPage({super.key});
 
@@ -10,21 +9,33 @@ class BarberMainPage extends StatefulWidget {
 
 class _BarberMainPageState extends State<BarberMainPage> {
   int pageNum = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(child: IndexedStack(
-            index: pageNum,
-            children: [
-              Placeholder(),
-              Placeholder(),
-              Placeholder(),
-              Placeholder(),
-            ],
-          )),
-          Align(child: Container())
+          Positioned.fill(
+            child: IndexedStack(
+              index: pageNum,
+              children: [
+                Placeholder(),
+                Placeholder(),
+                Placeholder(),
+                Placeholder(),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Colors.black,
+              ),
+              child: Row(children: []),
+            ),
+          ),
         ],
       ),
     );
