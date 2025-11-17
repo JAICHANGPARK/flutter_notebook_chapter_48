ee8f937c4475d0a8be1197293001d77bd7ab55c8
JAICHANGPARK <aristojeff@gmail.com>
2025-11-17 17:07:06 +0900

    Create barber_main_page.dart

---
+++ lib/ep2431_barber_booking_app/ui/barber_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BarberMainPage extends StatefulWidget {
+  const BarberMainPage({super.key});
+
+  @override
+  State<BarberMainPage> createState() => _BarberMainPageState();
+}
+
+class _BarberMainPageState extends State<BarberMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
