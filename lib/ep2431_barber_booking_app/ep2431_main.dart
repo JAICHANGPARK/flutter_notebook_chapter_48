eef1e1445103e389bbbcace5396cc7b735fa9959
JAICHANGPARK <aristojeff@gmail.com>
2025-11-17 17:07:28 +0900

    Update ep2431_main.dart

--- lib/ep2431_barber_booking_app/ep2431_main.dart
+++ lib/ep2431_barber_booking_app/ep2431_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_48/ep2431_barber_booking_app/barber_booking_app.dart';
 
 void main() {
-  
+  runApp(BarberBookingApp());
 }
