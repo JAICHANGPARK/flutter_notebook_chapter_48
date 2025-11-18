be19407ab282044bfdc4a70e45633436c6829510
JAICHANGPARK <aristojeff@gmail.com>
2025-11-18 18:25:13 +0900

    Create barber_search_page.dart

---
+++ lib/ep2431_barber_booking_app/ui/barber_search_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart'
+
+class BarberSearchPage extends StatefulWidget {
+  const BarberSearchPage({super.key});
+
+  @override
+  State<BarberSearchPage> createState() => _BarberSearchPageState();
+}
+
+class _BarberSearchPageState extends State<BarberSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
