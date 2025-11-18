362ef346f7333f8a0444aac867e74653e4b6a88f
JAICHANGPARK <aristojeff@gmail.com>
2025-11-18 18:28:13 +0900

    Update barber_search_page.dart

--- lib/ep2431_barber_booking_app/ui/barber_search_page.dart
+++ lib/ep2431_barber_booking_app/ui/barber_search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_48/ep2431_barber_booking_app/barber_theme.dart';
 
 class BarberSearchPage extends StatefulWidget {
   const BarberSearchPage({super.key});
@@ -10,20 +11,32 @@ class BarberSearchPage extends StatefulWidget {
 class _BarberSearchPageState extends State<BarberSearchPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: ShapeDecoration(shape: StadiumBorder()),
-              ),
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              spacing: 12,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: ShapeDecoration(shape: StadiumBorder()),
+                  ),
+                ),
+                CircleAvatar(
+                  radius: 24,
+                  backgroundColor: accentColor,
+                  child: Icon(Icons.tune),
+                  foregroundColor: Colors.black,
+                ),
+                CircleAvatar(radius: 24, backgroundColor: Colors.white24),
+              ],
             ),
-            CircleAvatar()
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
