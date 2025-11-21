86be38acddf78dcbf56016556d314d97bc7d0856
JAICHANGPARK <aristojeff@gmail.com>
2025-11-22 08:12:32 +0900

    Update barber_book_appointment.dart

--- lib/ep2431_barber_booking_app/ui/barber_book_appointment.dart
+++ lib/ep2431_barber_booking_app/ui/barber_book_appointment.dart
@@ -10,6 +10,12 @@ class BarberBookAppointment extends StatefulWidget {
 class _BarberBookAppointmentState extends State<BarberBookAppointment> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
