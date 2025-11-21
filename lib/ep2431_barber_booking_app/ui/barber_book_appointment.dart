a761714cf733683b5de7f9f39808f595d83a39c7
JAICHANGPARK <aristojeff@gmail.com>
2025-11-22 08:20:23 +0900

    Update barber_book_appointment.dart

--- lib/ep2431_barber_booking_app/ui/barber_book_appointment.dart
+++ lib/ep2431_barber_booking_app/ui/barber_book_appointment.dart
@@ -66,7 +66,7 @@ class _BarberBookAppointmentState extends State<BarberBookAppointment> {
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          spacing: 12,
+                          spacing: 8,
                           children: [
                             CircleAvatar(radius: 30),
                             Text(
