e6e3aa5f0482c65eab3cd8832020f014908955e8
JAICHANGPARK <aristojeff@gmail.com>
2025-11-22 08:12:16 +0900

    Create barber_book_appointment.dart

---
+++ lib/ep2431_barber_booking_app/ui/barber_book_appointment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BarberBookAppointment extends StatefulWidget {
+  const BarberBookAppointment({super.key});
+
+  @override
+  State<BarberBookAppointment> createState() => _BarberBookAppointmentState();
+}
+
+class _BarberBookAppointmentState extends State<BarberBookAppointment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
