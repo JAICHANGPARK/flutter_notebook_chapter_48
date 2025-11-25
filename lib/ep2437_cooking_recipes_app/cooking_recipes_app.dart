c09c51698aaf86174866c3fd2014592bab575d0e
JAICHANGPARK <aristojeff@gmail.com>
2025-11-25 18:41:12 +0900

    Update cooking_recipes_app.dart

--- lib/ep2437_cooking_recipes_app/cooking_recipes_app.dart
+++ lib/ep2437_cooking_recipes_app/cooking_recipes_app.dart
@@ -5,6 +5,8 @@ class CookingRecipesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      
+    );
   }
 }
