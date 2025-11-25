13d96096be3eaddb70b90ad41a52f8a48acf7b97
JAICHANGPARK <aristojeff@gmail.com>
2025-11-25 18:42:20 +0900

    Update cooking_recipes_app.dart

--- lib/ep2437_cooking_recipes_app/cooking_recipes_app.dart
+++ lib/ep2437_cooking_recipes_app/cooking_recipes_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'ui/cooking_recipes_main_page.dart';
+
 class CookingRecipesApp extends StatelessWidget {
   const CookingRecipesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: CookingRecipesMainPage(),
     );
   }
 }
