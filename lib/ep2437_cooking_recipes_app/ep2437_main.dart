7d0a6e5251ada317e08879636ade72b141ed2ab5
JAICHANGPARK <aristojeff@gmail.com>
2025-11-25 18:42:56 +0900

    Update ep2437_main.dart

--- lib/ep2437_cooking_recipes_app/ep2437_main.dart
+++ lib/ep2437_cooking_recipes_app/ep2437_main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_48/ep2437_cooking_recipes_app/cooking_recipes_app.dart';
 
-void main() {
-
-}
+void main() => runApp(CookingRecipesApp());
