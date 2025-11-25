7fe573b272fe4abe719715e8c971549d1361fc02
JAICHANGPARK <aristojeff@gmail.com>
2025-11-25 18:41:50 +0900

    Update cooking_recipes_main_page.dart

---
+++ lib/ep2437_cooking_recipes_app/ui/cooking_recipes_main_page.dart
@@ -0,0 +1,15 @@
+
+
+class CookingRecipesMainPage extends StatefulWidget {
+  const CookingRecipesMainPage({super.key});
+
+  @override
+  State<CookingRecipesMainPage> createState() => _CookingRecipesMainPageState();
+}
+
+class _CookingRecipesMainPageState extends State<CookingRecipesMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
