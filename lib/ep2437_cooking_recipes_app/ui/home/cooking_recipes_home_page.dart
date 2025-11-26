c82438ea8026b9c94ee1c786e1b1f58db344c89c
JAICHANGPARK <aristojeff@gmail.com>
2025-11-26 18:20:37 +0900

    Update cooking_recipes_home_page.dart

---
+++ lib/ep2437_cooking_recipes_app/ui/home/cooking_recipes_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class CookingRecipesHomePage extends StatefulWidget {
+  const CookingRecipesHomePage({super.key});
+
+  @override
+  State<CookingRecipesHomePage> createState() => _CookingRecipesHomePageState();
+}
+
+class _CookingRecipesHomePageState extends State<CookingRecipesHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
