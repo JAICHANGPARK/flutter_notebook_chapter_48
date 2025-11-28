29cae23d1ba0f5aef66682c9690004458f2a0ffc
JAICHANGPARK <aristojeff@gmail.com>
2025-11-28 17:29:10 +0900

    Update cooking_recipes.dart

--- lib/ep2437_cooking_recipes_app/data/cooking_recipes.dart
+++ lib/ep2437_cooking_recipes_app/data/cooking_recipes.dart
@@ -1,11 +1,24 @@
-
 class CookingRecipes {
   final int id;
   final String name;
   final String description;
   final String imageUrl;
 
-  CookingRecipes(
+  CookingRecipes({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.imageUrl,
+  });
 
-      this.id, this.name, this.description, this.imageUrl);
+  factory CookingRecipes.fromJson(Map<String, dynamic> json) {
+    return CookingRecipes(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      description: json['description'] as String? ?? '',
+      imageUrl: json['image_url'] as String,
+    );
+  }
+  
+
 }
