5d0b3ee3202e28ae32954ef618e2b7a6e0ce82f2
JAICHANGPARK <aristojeff@gmail.com>
2025-11-28 16:55:16 +0900

    Update cooking_recipes_home_page.dart

--- lib/ep2437_cooking_recipes_app/ui/home/cooking_recipes_home_page.dart
+++ lib/ep2437_cooking_recipes_app/ui/home/cooking_recipes_home_page.dart
@@ -69,10 +69,14 @@ class _CookingRecipesHomePageState extends State<CookingRecipesHomePage> {
                   padding: .symmetric(horizontal: 12),
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    boxShadow: [BoxShadow(color: Colors.grey[300]!,
-                    spreadRadius: 1,offset: Offset(0, 2),
-                      blurRadius: 3
-                    )],
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[300]!,
+                        spreadRadius: 1,
+                        offset: Offset(0, 2),
+                        blurRadius: 3,
+                      ),
+                    ],
                   ),
                   child: Row(
                     children: [
@@ -81,6 +85,7 @@ class _CookingRecipesHomePageState extends State<CookingRecipesHomePage> {
                         child: TextField(
                           decoration: InputDecoration(
                             hintText: "Search any recipe...",
+                            border: InputBorder.none,
                           ),
                         ),
                       ),
