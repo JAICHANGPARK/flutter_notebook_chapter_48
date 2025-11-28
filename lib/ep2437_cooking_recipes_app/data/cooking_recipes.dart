aef63bae0bd32cf2bfdb1c2b72a54fb3583988ff
JAICHANGPARK <aristojeff@gmail.com>
2025-11-28 17:27:10 +0900

    Update cooking_recipes.dart

---
+++ lib/ep2437_cooking_recipes_app/data/cooking_recipes.dart
@@ -0,0 +1,11 @@
+
+class CookingRecipes {
+  final int id;
+  final String name;
+  final String description;
+  final String imageUrl;
+
+  CookingRecipes(
+
+      this.id, this.name, this.description, this.imageUrl);
+}
