e826115da9ae82058646e2a93e379b027287271d
JAICHANGPARK <aristojeff@gmail.com>
2025-11-29 09:14:58 +0900

    Update cooking_recipes_detail_page.dart

--- lib/ep2437_cooking_recipes_app/ui/recipe/cooking_recipes_detail_page.dart
+++ lib/ep2437_cooking_recipes_app/ui/recipe/cooking_recipes_detail_page.dart
@@ -29,9 +29,16 @@ class _CookingRecipesDetailPageState extends State<CookingRecipesDetailPage> {
                 Icon(Icons.alarm_add),
                 Text("10 min"),
                 Icon(Icons.bar_chart),
-                Text("Medium")
+                Text("Medium"),
               ],
-            )
+            ),
+            Container(height: 320, child: Placeholder()),
+            Text("Description"),
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              maxLines: 2,
+            ),
+            ExpansionTile(title: Text("Ingredients")),
           ],
         ),
       ),
