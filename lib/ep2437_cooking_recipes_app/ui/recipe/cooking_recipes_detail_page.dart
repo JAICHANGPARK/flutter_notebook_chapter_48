87be99f5d78b0ad192780f9aaca3c387284a6406
JAICHANGPARK <aristojeff@gmail.com>
2025-11-29 09:12:55 +0900

    Update cooking_recipes_detail_page.dart

--- lib/ep2437_cooking_recipes_app/ui/recipe/cooking_recipes_detail_page.dart
+++ lib/ep2437_cooking_recipes_app/ui/recipe/cooking_recipes_detail_page.dart
@@ -12,17 +12,21 @@ class _CookingRecipesDetailPageState extends State<CookingRecipesDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(children: [
-        SizedBox(
-          height: 52,
-
-          child: Stack(children: [
-          Placeholder()
-          ],),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 52, child: Stack(children: [Placeholder()])),
+            Row(
+              children: [
+                Text("Choco Macarons"),
+                Spacer(),
+                Icon(Icons.star),
+                Text("4.7"),
+              ],
+            ),
+          ],
         ),
-
-
-      ])),
+      ),
     );
   }
 }
