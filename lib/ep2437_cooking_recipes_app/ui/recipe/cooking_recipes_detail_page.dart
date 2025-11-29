b038ae129b2d3db0cd24b446b419afc49c64318c
JAICHANGPARK <aristojeff@gmail.com>
2025-11-29 09:09:19 +0900

    Update cooking_recipes_detail_page.dart

--- lib/ep2437_cooking_recipes_app/ui/recipe/cooking_recipes_detail_page.dart
+++ lib/ep2437_cooking_recipes_app/ui/recipe/cooking_recipes_detail_page.dart
@@ -11,6 +11,10 @@ class CookingRecipesDetailPage extends StatefulWidget {
 class _CookingRecipesDetailPageState extends State<CookingRecipesDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(child: Column(children: [
+
+      ],)),
+    );
   }
 }
