cfd0160ebc87cf7cc83b5b7a970c8a1dc4beb434
JAICHANGPARK <aristojeff@gmail.com>
2025-12-01 18:53:46 +0900

    Update e_commerce_app.dart

--- lib/ep2444_e_commerce_app/e_commerce_app.dart
+++ lib/ep2444_e_commerce_app/e_commerce_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ui/e_commerce_home_page.dart';
+
 
 class ECommerceApp extends StatelessWidget {
   const ECommerceApp({super.key});
@@ -7,7 +9,7 @@ class ECommerceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: ECommerceHomePage(),
     );
   }
 }
