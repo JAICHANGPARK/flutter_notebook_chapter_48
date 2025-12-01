99e71ffe62ce51daa2e3bf7d7630ae834ac35db9
JAICHANGPARK <aristojeff@gmail.com>
2025-12-01 18:52:50 +0900

    Update e_commerce_home_page.dart

--- lib/ep2444_e_commerce_app/ui/e_commerce_home_page.dart
+++ lib/ep2444_e_commerce_app/ui/e_commerce_home_page.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+
+class ECommerceHomePage extends StatefulWidget {
+  const ECommerceHomePage({super.key});
+
+  @override
+  State<ECommerceHomePage> createState() => _ECommerceHomePageState();
+}
+
+class _ECommerceHomePageState extends State<ECommerceHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
