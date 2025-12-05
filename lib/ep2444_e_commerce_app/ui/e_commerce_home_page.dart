3d4053a8cf3238db5b648652dda4f59558bdb6d6
JAICHANGPARK <aristojeff@gmail.com>
2025-12-05 16:14:19 +0900

    Update e_commerce_home_page.dart

--- lib/ep2444_e_commerce_app/ui/e_commerce_home_page.dart
+++ lib/ep2444_e_commerce_app/ui/e_commerce_home_page.dart
@@ -273,155 +273,144 @@ class _ECommerceHomePageState extends State<ECommerceHomePage> {
                                     ],
                                   ),
                                 ),
-                                Padding(
-                                  padding: .only(right: 16),
-                                  child: IndexedStack(
-                                    index: selectedTab,
-                                    children: [
-                                      GridView.builder(
-                                        shrinkWrap: true,
-                                        padding: .zero,
-                                        gridDelegate:
-                                            SliverGridDelegateWithFixedCrossAxisCount(
-                                              crossAxisCount: 2,
-                                              mainAxisSpacing: 8,
-                                              crossAxisSpacing: 8,
-                                              childAspectRatio: .7,
-                                            ),
-                                        itemCount: 10,
-                                        itemBuilder: (context, index) {
-                                          return Container(
-                                            height: 360,
-                                            margin: .only(bottom: 12),
-                                            decoration: ShapeDecoration(
-                                              shape: RoundedSuperellipseBorder(
-                                                borderRadius: .circular(16),
+                                GridView.builder(
+                                  shrinkWrap: true,
+                                  padding: .zero,
+                                  gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    mainAxisSpacing: 8,
+                                    crossAxisSpacing: 8,
+                                    childAspectRatio: .7,
+                                  ),
+                                  itemCount: 10,
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      height: 360,
+                                      margin: .only(bottom: 12),
+                                      decoration: ShapeDecoration(
+                                        shape: RoundedSuperellipseBorder(
+                                          borderRadius: .circular(16),
+                                        ),
+                                        color: .fromRGBO(
+                                          237,
+                                          241,
+                                          244,
+                                          1,
+                                        ),
+                                      ),
+                                      child: Stack(
+                                        children: [
+                                          Positioned.fill(
+                                            child: Padding(
+                                              padding:
+                                              const EdgeInsets.all(
+                                                12.0,
                                               ),
-                                              color: .fromRGBO(
-                                                237,
-                                                241,
-                                                244,
-                                                1,
-                                              ),
-                                            ),
-                                            child: Stack(
-                                              children: [
-                                                Positioned.fill(
-                                                  child: Padding(
-                                                    padding:
-                                                        const EdgeInsets.all(
-                                                          12.0,
+                                              child: Column(
+                                                spacing: 12,
+                                                children: [
+                                                  Expanded(
+                                                    child: Container(
+                                                      decoration:
+                                                      BoxDecoration(
+                                                        color: Colors
+                                                            .white,
+                                                        borderRadius:
+                                                            .circular(
+                                                          16,
                                                         ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  Container(
+                                                    decoration:
+                                                    BoxDecoration(
+                                                      color: Colors
+                                                          .white,
+                                                      borderRadius:
+                                                          .circular(
+                                                        16,
+                                                      ),
+                                                    ),
+                                                    padding: .symmetric(
+                                                      horizontal: 12,
+                                                      vertical: 12,
+                                                    ),
                                                     child: Column(
-                                                      spacing: 12,
+                                                      spacing: 4,
                                                       children: [
-                                                        Expanded(
-                                                          child: Container(
-                                                            decoration:
-                                                                BoxDecoration(
-                                                                  color: Colors
-                                                                      .white,
-                                                                  borderRadius:
-                                                                      .circular(
-                                                                        16,
-                                                                      ),
-                                                                ),
-                                                          ),
+                                                        Row(
+                                                          mainAxisAlignment:
+                                                              .spaceBetween,
+                                                          children: [
+                                                            Text(
+                                                              "Men's Pullover",
+                                                              style: TextStyle(
+                                                                fontSize:
+                                                                16,
+                                                              ),
+                                                            ),
+                                                            // Text("Price"),
+                                                          ],
                                                         ),
-                                                        Container(
-                                                          decoration:
-                                                              BoxDecoration(
-                                                                color: Colors
-                                                                    .white,
-                                                                borderRadius:
-                                                                    .circular(
-                                                                      16,
-                                                                    ),
+                                                        Row(
+                                                          mainAxisAlignment:
+                                                              .spaceBetween,
+                                                          children: [
+                                                            Text(
+                                                              "Hoodie",
+                                                              style: TextStyle(
+                                                                fontSize:
+                                                                16,
                                                               ),
-                                                          padding: .symmetric(
-                                                            horizontal: 12,
-                                                            vertical: 12,
-                                                          ),
-                                                          child: Column(
-                                                            spacing: 4,
-                                                            children: [
-                                                              Row(
-                                                                mainAxisAlignment:
-                                                                    .spaceBetween,
-                                                                children: [
-                                                                  Text(
-                                                                    "Men's Pullover",
-                                                                    style: TextStyle(
-                                                                      fontSize:
-                                                                          16,
-                                                                    ),
-                                                                  ),
-                                                                  // Text("Price"),
-                                                                ],
+                                                            ),
+                                                            Text(
+                                                              "\$199.00",
+                                                              style: TextStyle(
+                                                                fontSize:
+                                                                16,
                                                               ),
-                                                              Row(
-                                                                mainAxisAlignment:
-                                                                    .spaceBetween,
-                                                                children: [
-                                                                  Text(
-                                                                    "Hoodie",
-                                                                    style: TextStyle(
-                                                                      fontSize:
-                                                                          16,
-                                                                    ),
-                                                                  ),
-                                                                  Text(
-                                                                    "\$199.00",
-                                                                    style: TextStyle(
-                                                                      fontSize:
-                                                                          16,
-                                                                    ),
-                                                                  ),
-                                                                ],
-                                                              ),
-                                                            ],
-                                                          ),
+                                                            ),
+                                                          ],
                                                         ),
                                                       ],
                                                     ),
                                                   ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                          Positioned(
+                                            right: 4,
+                                            top: 4,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                color: .fromRGBO(
+                                                  237,
+                                                  241,
+                                                  244,
+                                                  1,
                                                 ),
-                                                Positioned(
-                                                  right: 4,
-                                                  top: 4,
-                                                  child: Container(
-                                                    decoration: BoxDecoration(
-                                                      color: .fromRGBO(
-                                                        237,
-                                                        241,
-                                                        244,
-                                                        1,
-                                                      ),
-                                                      borderRadius: .only(
-                                                        bottomLeft: .circular(
-                                                          24,
-                                                        ),
-                                                      ),
-                                                    ),
-                                                    padding: .all(16),
-                                                    child: Icon(
-                                                      Icons.favorite_border,
-                                                      size: 18,
-                                                    ),
+                                                borderRadius: .only(
+                                                  bottomLeft: .circular(
+                                                    24,
                                                   ),
                                                 ),
-                                              ],
+                                              ),
+                                              padding: .all(16),
+                                              child: Icon(
+                                                Icons.favorite_border,
+                                                size: 18,
+                                              ),
                                             ),
-                                          );
-                                        },
+                                          ),
+                                        ],
                                       ),
-                                      Placeholder(),
-                                      Placeholder(),
-                                      Placeholder(),
-                                      Placeholder(),
-                                    ],
-                                  ),
+                                    );
+                                  },
                                 ),
+                                
                               ],
                             ),
                           ),
