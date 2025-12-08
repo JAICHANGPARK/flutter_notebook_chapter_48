f773f06ab3bccc8a00ac9767576010ca2b73417b
JAICHANGPARK <aristojeff@gmail.com>
2025-12-08 18:43:08 +0900

    Update e_commerce_detail_page.dart

--- lib/ep2444_e_commerce_app/ui/e_commerce_detail_page.dart
+++ lib/ep2444_e_commerce_app/ui/e_commerce_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ECommerceDetailPage extends StatefulWidget {
   const ECommerceDetailPage({super.key});
@@ -269,6 +270,7 @@ class _ECommerceDetailPageState extends State<ECommerceDetailPage> {
                                     }).toList(),
                               ),
                             ),
+                            Gap(2),
                             Text("Description", style: TextStyle(fontSize: 16)),
                             Text(
                               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
