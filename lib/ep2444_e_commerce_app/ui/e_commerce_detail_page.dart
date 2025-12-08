f5294f49d5c5dc63a603fc677775b178fc5819b8
JAICHANGPARK <aristojeff@gmail.com>
2025-12-08 18:43:31 +0900

    Update e_commerce_detail_page.dart

--- lib/ep2444_e_commerce_app/ui/e_commerce_detail_page.dart
+++ lib/ep2444_e_commerce_app/ui/e_commerce_detail_page.dart
@@ -271,10 +271,17 @@ class _ECommerceDetailPageState extends State<ECommerceDetailPage> {
                               ),
                             ),
                             Gap(2),
-                            Text("Description", style: TextStyle(fontSize: 16)),
-                            Text(
-                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                            ),
+                            Column(
+                              crossAxisAlignment: .start,
+                              spacing: 8,
+                              children: [
+                                Text("Description", style: TextStyle(fontSize: 16)),
+                                Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                ),
+                              ],
+                            )
+
                           ],
                         ),
                       ),
